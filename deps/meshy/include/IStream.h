4250c19631d872ff6e284a62930c7ca2db77c9c8
feifeigd <feifeigd@21cn.com>
2017-04-16 22:55:09 +0800

    istream

---
+++ deps/meshy/include/IStream.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <functional>
+
+namespace meshy {
+	class IStream
+	{
+	public:
+		typedef std::function<void(char const* buf, size_t size)> DataIndicationHandler;
+
+		virtual size_t receive(char* buffer, size_t bufferSize, size_t& readSize) = 0;
+		virtual size_t send(ByteArray const& byteArray) = 0;
+
+		virtual void OnDataIndication(DataIndicationHandler handler) = 0;
+		virtual DataIndicationHandler GetDataIndication() = 0;
+	};
+}

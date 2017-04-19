fc892f88de031547339081295694ad592548d273
feifeigd <feifeigd@21cn.com>
2017-04-20 00:09:08 +0800

    modify ByteArray.h

--- deps/meshy/include/ByteArray.h
+++ deps/meshy/include/ByteArray.h
@@ -1,14 +1,16 @@
 #pragma once
 
 #include <vector>
+#include <cstdint>
+#include <string>
 
 namespace meshy {
 	class ByteArray : public std::vector<char>
 	{
 	public:
 		ByteArray() = default;
-		ByteArray(size_t size);
-		ByteArray(char const* buffer, size_t size);
+		ByteArray(std::size_t size);
+		ByteArray(char const* buffer, std::size_t size);
 		ByteArray(std::string const& data);
 
 		ByteArray& concat(ByteArray const& data);

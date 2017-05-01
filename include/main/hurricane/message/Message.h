25a814a24d4a5ced4e1e616a1e9a71869624df11
feifeigd <feifeigd@21cn.com>
2017-05-01 10:56:08 +0800

    换目录

---
+++ include/main/hurricane/message/Message.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace hurricane {
+	namespace message {
+
+		class Message {
+		public:
+			struct Type
+			{
+				enum {
+					Stop = 0,
+				};
+			};
+
+			Message(int32_t type) : m_type(type) {}
+
+			int32_t type()const { return m_type; }
+			void type(int32_t type) { m_type = type; }
+
+		private:
+			int32_t	m_type;
+		};
+	}
+}

c9d405b5b9a96df3f7d2d64ff08f69388e32bc2a
luofei <13632323692@139.com>
2017-11-23 12:33:00 +0800

    添加服务器例子

---
+++ deps/meshy/include/EchoSink.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "DataSink.h"
+#include <string>
+#include <cassert>
+#include "utils/logger.h"
+
+class EchoSink : public meshy::DataSink{
+public:
+	/// 收到了对端的一段数据
+	virtual size_t OnDataIndication(meshy::IStream* stream, char const* buf, size_t bytes){
+		assert(stream && buf);
+		TRACE_DEBUG("Receive Data:%s", buf);
+		return stream->send({ buf, bytes });		
+	}
+};
---
+++ test/EchoServer/src/EchoServer.cpp
@@ -0,0 +1,17 @@
+﻿// TestTcpServer.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <EchoSink.h>
+
+int main()
+{
+	EchoSink sink;
+	meshy::TcpServer server(&sink);
+	server.listen("0.0.0.0", 9000);
+	bool bRun = true;
+	while (bRun)
+		;
+    return 0;
+}
+
